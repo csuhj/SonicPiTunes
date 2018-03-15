338491620c3ff5277e33f88aa894e3b2d75ada2a
Adam Robertson <adam.robertson@apteco.com>
2018-03-15 13:20:36 +0000

    whoops - hadn't saved the final version of this

--- Imperial March/Imperial March Melody Bass Drums.rb
+++ Imperial March/Imperial March Melody Bass Drums.rb
@@ -9,12 +9,13 @@ qu=bar_length/8.0  #quaver - eighth note
 sq=bar_length/16.0 #semiquaver - sixteenth note
 
 live_loop :loop_drums do
-  sample :loop_amen, beat_stretch: tempo
-  sleep tempo
+  sample :loop_amen, beat_stretch: bar_length, amp: 1
+  sleep bar_length
 end
 
 live_loop :loop_bass do
   use_synth :pluck
+  use_synth_defaults amp: 1
   play_pattern_timed [:G2, :r, :G2, :r, :G3, :r, :G2, :r], [cr, cr, cr, cr, cr, cr, cr, cr]
   play_pattern_timed [:D3, :r, :D3, :r, :Fs2, :r, :G2, :r], [cr, cr, cr, cr, cr, cr, cr, cr]
   play_pattern_timed [:G3, :r, :G3, :r, :E3, :r, :Cs3, :r], [cr, cr, cr, cr, cr, cr, cr, cr]
@@ -22,7 +23,8 @@ live_loop :loop_bass do
 end
 
 live_loop :loop_melody do
-  use_synth :blade
+  use_synth :pulse
+  use_synth_defaults amp: 0.3
   play_pattern_timed [:G, :G, :G, :Eb, :r, :Bb, :G, :Eb, :r, :Bb, :G], [cr, cr, cr, qu, sq, sq, cr, qu, sq, sq, mi]
   play_pattern_timed [:D5, :D5, :D5, :Ds5, :r, :As, :Fs, :Eb, :r, :Bb, :G], [cr, cr, cr, qu, sq, sq, cr, qu, sq, sq, mi]
   play_pattern_timed [:G5, :G, :r, :G, :G5, :Fs5, :r], [cr, qu, sq, sq, cr, sq, sq]
